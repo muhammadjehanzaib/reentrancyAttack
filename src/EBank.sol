be8702911d97d13344604128e3ae618326019c1b
Soulsniper <soulsniper155@gmail.com>
2025-01-01 21:55:25 +0500

    E bank smart contract valut

---
+++ src/EBank.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract EBank {
+    mapping(address => uint256) balances;
+
+    event AmountDeposited(address user, uint256 amountDeposit);
+    event withdrawAmount(address user, uint256 amountWithdraw);
+
+    function deposit() external payable {
+        require(msg.value > 0, "Amount Should be greater than Zero.");
+        balances[msg.sender] = balances[msg.sender] + msg.value;
+        emit AmountDeposited(msg.sender, msg.value);
+    }
+
+    function withdraw(uint256 amountToWithdraw) external {
+        require(
+            amountToWithdraw > 0 && balances[msg.sender] >= amountToWithdraw,
+            "Amount and deposit Balance should be greater than zero"
+        );
+
+        (bool success,) = msg.sender.call{value: amountToWithdraw}("");
+        require(success, "Transfer failed");
+
+        balances[msg.sender] = balances[msg.sender] - amountToWithdraw;
+        emit withdrawAmount(msg.sender, amountToWithdraw);
+    }
+
+    function getAccountBalance(address user) public view returns (uint256) {
+        return balances[user];
+    }
+}
